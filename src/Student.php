de41ab7766bfe6aef60ba25676d78b3ea245a9c9
Sam Martinez & Mike Chastain <student@epicodus.com>
2015-08-25 10:13:22 -0700

    add constructors getters and setters to student class

--- src/Student.php
+++ src/Student.php
@@ -16,6 +16,32 @@
 
       //Setters
 
+      function setStudentName($new_student_name)
+      {
+          $this->student_name = (string) $new_student_name;
+      }
+
+      function setDateOfEnrollment($new_date_of_enrollment)
+      {
+          $this->date_of_enrollment = $new_date_of_enrollment;
+      }
+
+      //Getters
+
+      function getStudentName()
+      {
+          return $this->student_name;
+      }
+
+      function getDateOfEnrollment()
+      {
+          return $this->date_of_enrollment;
+      }
+
+      function getId()
+      {
+          return $this->id;
+      }
 
     }
 
