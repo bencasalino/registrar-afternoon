2e720f30309dacfb92e75e7f8620d8859ed0d194
ben & chris <student@epicodus.com>
2015-08-25 16:23:37 -0700

    add edit student html twig and add edit course html twig

--- app/app.php
+++ app/app.php
@@ -17,14 +17,42 @@
 
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
+    // needed for intial set up everything above this line
 
+
+    // link to Home page
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array('courses' => Course::getAll(), 'students' => Student::getAll()));
    });
 
+   // link to courses twig views page
    $app->get("/courses", function() use($app){
        return $app['twig']->render('courses.html.twig', array('courses'=> Course::getAll(), 'students'=> Student::getAll()));
    });
 
+   // link to courses twig views page
+   $app->get("/students", function() use($app){
+       return $app['twig']->render('students.html.twig', array('students'=> Student::getAll(), 'courses'=> Course::getAll()));
+   });
+
+    // path to edit individual student
+    // $app->get("/students/{id}", function($id) use($app) {
+    //     $student = Student::find($id);
+    //     return $app['twig']->render('student_edit.html.twig', array('student'=>$student, 'students'=> $course->getStudents()));
+
+    // path to update individual student
+    // $app->patch
+
+    // post edited student information
+    // $app-> post
+
+
+
+
+
+
+
+
+        // must have a return app at the bottom of app.php page
         return $app;
 ?>
